3da99f2a8e05b47e8f28d9da4bc06ee150666487
bdushimi <lespharaons@hotmail.com>
2021-07-22 13:06:21 +0200

    Food blog initial setup

--- script.js
+++
@@ -1,6 +0,0 @@
-function toggleMenu(){
-    document.getElementById("primaryNav").classList.toggle("open")
-}
-
-let hamburgerBtn = document.getElementById("hamburgerBtn");
-hamburgerBtn.onclick = toggleMenu ;
